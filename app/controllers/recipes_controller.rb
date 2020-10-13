6d95fdbd6d5da66b817dd926cbf308dc3cf61932
EC2 Default User <kapibara.bba0924@gmail.com>
2020-10-13 05:40:50 +0000

    [Add]recipe2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,7 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
-  
+
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
@@ -20,10 +20,29 @@ class RecipesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user_id != current_user.id
+      redirect_to recipes_path, alert: '不正なアクセスです。'
+    end
   end
-  
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe), notice: "更新に成功しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:title, :body)
